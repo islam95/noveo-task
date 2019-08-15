a3b267e22307940a0f6b01812a26439e7235ede9
Islam Dudaev <islamwolf23@gmail.com>
2019-08-15 20:16:42 +0100

    Add comment to the disk action creator

--- src/redux/actions/disk.js
+++ src/redux/actions/disk.js
@@ -1,6 +1,7 @@
 import { LOAD_DISK } from './types';
 import * as api from '../../helpers/api';
 
+// Get all items from api call and save them to redux store
 export const getFiles = (token, path) => {
   return async dispatch => {
     const files = await api.getFiles(token, path);
