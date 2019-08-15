f98b241aab11be65d1ee6eea71b8e75fea9296fd
Islam Dudaev <islamwolf23@gmail.com>
2019-08-15 16:18:47 +0100

    Add rootReducer

---
+++ src/redux/store/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import auth from '../reducers/authReducer';
+import disk from '../reducers/diskReducer';
+
+const rootReducer = combineReducers({
+  auth,
+  disk
+});
+
+export default rootReducer;
