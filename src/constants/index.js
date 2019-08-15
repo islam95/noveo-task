18209cfc5f32605f0ae00b7d565326fd9f3470db
Islam Dudaev <islamwolf23@gmail.com>
2019-08-15 19:12:34 +0100

    Delete not used files

--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const clientID = "cab2f01f49b24988995f70f3ecd2b636";
-//export const myAccessCode = "AgAAAAANaiRLAAXTKssrYovTF0HwhNm7Xg06nPE";
-export const baseURL = "https://oauth.yandex.ru/authorize?response_type=token&client_id=cab2f01f49b24988995f70f3ecd2b636";
-export const cloudApi = "https://cloud-api.yandex.net/v1/disk/";
-
-export const callbackUrl = "http://127.0.0.1:3000/files";
-
