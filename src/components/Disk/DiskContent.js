cab1b3bd297e66a3f66c5ad3bb9c8b17f94d6b50
Islam Dudaev <islamwolf23@gmail.com>
2019-08-16 18:51:34 +0100

    Create DiskContent component to print disk files

---
+++ src/components/Disk/DiskContent.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { formatBytes, removeDiskColumn } from "../../helpers/helpers";
+import { FaFolder } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const DiskContent = (props) => (
+  <tbody>
+    {props.files.items.map((item, i) => {
+      const { name, path, type, size } = item;
+      const newPath = removeDiskColumn(path);
+      return (
+        <tr key={i}>
+          <td>
+            {type === "dir" ? (
+              <Link
+                to={`${newPath}`}
+                onClick={() => props.getDisk(name)}
+              >
+                <FaFolder /> {name}
+              </Link>
+            ) : (
+              name
+            )}
+          </td>
+          <td>{type === "file" && formatBytes(size)}</td>
+        </tr>
+      );
+    })}
+  </tbody>
+);
+
+export default DiskContent;
