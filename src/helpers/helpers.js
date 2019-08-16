03bc6991440a1b10574e79e933db388ff8b5139a
Islam Dudaev <islamwolf23@gmail.com>
2019-08-16 14:31:48 +0100

    Update formatBytes function by removing unneccessory code

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -5,12 +5,14 @@ export const parseAccessToken = url => {
 
 // Converts bytes to Kb
 export const formatBytes = (bytes, decimals = 2) => {
-  if (bytes === 0) return '0 Bytes';
+  if (bytes === 0) return "0 Bytes";
 
   const k = 1024;
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
 
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + ' ' + sizes[i];
-}
+  return (
+    parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + " " + sizes[i]
+  );
+};
