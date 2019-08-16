966f35af08a3c992e375b6e2e853a26500333de2
Islam Dudaev <islamwolf23@gmail.com>
2019-08-16 01:57:55 +0100

    Fix linting and buttons outline

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Nav, Navbar, NavItem, NavbarBrand, Button } from "reactstrap";
 
-const NavBar = ({token, login, logout}) => (
+const NavBar = ({ token, login, logout }) => (
   <Navbar color="light" light expand="md">
     <NavbarBrand>Yandex.disk</NavbarBrand>
     <Nav className="ml-auto" navbar>
       <NavItem>
         {!token ? (
-          <Button color="primary" onClick={login}>
+          <Button outline color="primary" onClick={login}>
             Login
           </Button>
         ) : (
-          <Button color="danger" onClick={logout}>
+          <Button outline color="danger" onClick={logout}>
             Logout
           </Button>
         )}
